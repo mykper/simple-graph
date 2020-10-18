c38235a6892dfa269e21bc0dc01f8f34a81d20ce
mykper <70755917+mykper@users.noreply.github.com>
2020-10-18 16:20:00 +0000

    Deploying to gh-pages from  @ e9c5b607021b7bdf993fbcc25fbc22073956e9ab 🚀

---
+++ precache-manifest.b6744cc118d7fcae26b8ce09ae67bc73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f12468f222107519a02dd21a3a25b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab8322360fe13e44baa",
+    "url": "/static/css/main.a48eb2e0.chunk.css"
+  },
+  {
+    "revision": "1826f582c00d60fceb88",
+    "url": "/static/js/2.b1f20951.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b1f20951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab8322360fe13e44baa",
+    "url": "/static/js/main.06882315.chunk.js"
+  },
+  {
+    "revision": "347a4c803077027da018",
+    "url": "/static/js/runtime-main.d6b22e42.js"
+  }
+]);

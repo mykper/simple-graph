42458bad4d303b005162322f8e58027020748d7a
mykper <70755917+mykper@users.noreply.github.com>
2020-10-18 16:24:07 +0000

    Deploying to gh-pages from  @ 7355f471a1eef70af72c091dbe6724668c7ffaf8 🚀

---
+++ precache-manifest.2a365f43f623174be6fe0799124c8d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16099f2783182c6a8a604b51541bc61",
+    "url": "/simple-graph/index.html"
+  },
+  {
+    "revision": "048d9fe78258c4ba8deb",
+    "url": "/simple-graph/static/css/main.a48eb2e0.chunk.css"
+  },
+  {
+    "revision": "1826f582c00d60fceb88",
+    "url": "/simple-graph/static/js/2.b1f20951.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-graph/static/js/2.b1f20951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048d9fe78258c4ba8deb",
+    "url": "/simple-graph/static/js/main.53947c5c.chunk.js"
+  },
+  {
+    "revision": "1fb806a3590e2b13c0fc",
+    "url": "/simple-graph/static/js/runtime-main.8955ad4e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6744cc118d7fcae26b8ce09ae67bc73.js"
+  "/simple-graph/precache-manifest.2a365f43f623174be6fe0799124c8d01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-graph/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
